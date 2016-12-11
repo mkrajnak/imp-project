7008d55ef4c34c42082162d268701404e8c6344a
mkrajnak <mkrajnak@redhat.com>
2016-12-11 04:29:48 +0100

    done

--- main.c
+++ main.c
@@ -51,14 +51,19 @@
 #include <fitkitlib.h>
 #include <keyboard/keyboard.h>
 #include <lcd/display.h>
+#include <limits.h>
 
+#define DISPLAY_DELAY 2     // delay is needed to display numbers higher than 9
+#define BUFFER_SIZE 128     // buffer for timestamps
+#define COUNT_TIME 10       // pariod of time
 
-#define DISPLAY_DELAY 5
-
-char last_ch; //naposledy precteny znak
-int char_cnt = 0;
-short display_overall = 0;
-int nums[10]= { //numbers on secondary 7 segment display
+char last_ch;                 // naposledy precteny znak
+int char_cnt = 0;             // all typed characters
+int timer = 11;               // counting from time from start
+int hits[BUFFER_SIZE] = {0};  // hits time stamps
+int hit_index = 0;            // index determines where are timestamps written
+short display_overall = 0;    // boolean switch between overall hits and hits/period of time
+int nums[10]= {               // numbers on secondary 7 segment display
   0xFC, //0 0110 0000
   0x60, //1 0x60
   0xDA, //2 0xDA
@@ -71,9 +76,10 @@ int nums[10]= { //numbers on secondary 7 segment display
   0xE6, //9
 };
 
-
-
-void print_user_help(void) { }
+void print_user_help(void)
+{
+  term_send_str_crlf("Keyboard hits counter");
+}
 
 unsigned char decode_user_cmd(char *cmd_ucase, char *cmd)
 {
@@ -84,7 +90,9 @@ void fpga_initialized()
 {
 }
 
-// show number on external 7 segment display
+/**
+* show number on external 7 segment display
+*/
 void displayNumber(int display) {
   if (display < 0)      // shut display down
   {
@@ -115,7 +123,7 @@ void displayNumber(int display) {
     P1OUT = 0x20;
     P6OUT = ~nums[display%10];
     delay_ms(DISPLAY_DELAY);
-  } 
+  }
   else if( display < 10000) // all of displays are operating
   {
     P1OUT = 0x04;
@@ -130,9 +138,11 @@ void displayNumber(int display) {
     P1OUT = 0x20;
     P6OUT = ~nums[display%10];
     delay_ms(DISPLAY_DELAY);
-  } 
+  }
 }
-
+/**
+* handle new hits
+*/
 int keyboard_idle()
 {
   char ch;
@@ -153,12 +163,65 @@ int keyboard_idle()
       }
       else{
         LCD_append_char(ch);
+        hits[hit_index] = timer;      // write timestamp
+        hit_index++;
+        if (hit_index == BUFFER_SIZE) // start over when needed
+        {
+          hit_index = 0;
+        }
         char_cnt++;
       }
     }
   }
-  
   return 0;
+}
+
+/**
+* Timer A initiated, information in comments are from MCU datasheet
+*/
+void init_timer(){
+  CCTL0 = CCIE;
+  TACCR0 = 32768;   // restart/start timer
+                    // timer is stopped
+                    // wirting a non-zero value will restart the timer
+  TACTL = (unsigned int)0x0100010110;
+  /*
+  01 - source - ACLK (32768 Hz)
+  00 - devided by 1
+  01 - mode up
+  0 - unused
+  1 - start counting from zero
+  1 - enable interrupt
+  0 - no interrupt pendding
+  */
+}
+
+/**
+* Timer A interrupt
+*/
+interrupt (TIMERA0_VECTOR) Timer_A (void){
+  timer++;
+  if (timer == INT_MAX) // just in case our application will be running for years
+  {
+    timer = 11;
+  }
+  return;
+}
+
+/**
+* go throgh time stamp buffer and make statistic
+*/
+int count_hits(){
+  int i;
+  int n = 0;
+  for (i = 0; i < BUFFER_SIZE; ++i)
+  {
+    if (hits[i] > (timer - COUNT_TIME))
+    {
+      n++;
+    }
+  }
+  return n;
 }
 
 int main(void)
@@ -170,6 +233,7 @@ int main(void)
   keyboard_init();
   LCD_init();
   LCD_clear();
+  init_timer();
 
   P1DIR |= 0xFF;
   P6DIR |= 0xFF;
@@ -181,7 +245,6 @@ int main(void)
     if (display_overall)
       displayNumber(char_cnt);
     else
-      displayNumber(-1);
-  }         
+      displayNumber(count_hits());
+  }
 }
-
